25323ad025188a4db47caeeab806c3f1c9f93917
Kevin Stobbs <Kevin.Stobbs94@gmail.com>
2023-02-09 20:44:45 +0000

    task complete

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const { response } = require('express');
 const express = require('express'); //npm install express
+const progLang = require('./assets/prog-lan.json');
 
 const app = express();
 
@@ -9,13 +9,38 @@ const PORT = 3000;
 
 // localhost:3000/test
 app.get('/test', (request, response) => {
-  response.send('alive!')
+response.send('alive!')
 })
 
 app.get('/',(request,response)=>{
-  response.status(200).send('home route');
+response.status(200).send('home route');
 })
 
+// localhost:/petslist
+let myPetsList = ['Odin', 'Arthas', 'Fudge'];
+app.get('/petsList',(request,response)=>{
+response.status(200).send(myPetsList)
+})
+
+// localhost:/prog-lan
+app.get('/prog-lan', (req, res) => {
+class ProgLanInfo {
+constructor(title, dateCreated, description, imageUrl) {
+this.title = title;
+this.dateCreated = dateCreated;
+this.description = description;
+this.imageUrl = imageUrl;
+}
+}
+
+try {
+const myProgLan = progLang.map(lang => new ProgLanInfo(lang.title, lang.dateCreated, lang.description, lang.imageUrl));
+res.status(200).send(myProgLan);
+} catch (error) {
+res.status(500).send(error)
+}
+});
+
 app.listen(PORT,()=>{
-  console.log(`Listening on PORT ${PORT}`);
+console.log(`Listening on PORT ${PORT}`);
 })
